9a7a05bdd5fc6a1338213f2ab5a7304c8aad1da7
Luis Torruella <letorruella@THICCBOOK.local>
2020-06-24 16:41:22 -0400

    added graph and api

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,19 @@
   <div id="app">
 
 
-    <h2>Page Views</h2>
-    <Plotly v-if="loaded" 
-    :data="pageViewData" 
-    :layout="layout" 
-    :display-mode-bar="false" />
-
-  <h2>Views by Category</h2>
+  <div :style="{
+    'display':'flex',
+    'justify-items':'end'
+  }">
+  <h2>{{menuItem}}</h2>
     <Plotly v-if="loaded"
-    :data="pageViewsByCategoryData"
+    sty
+    :data="nutritionByCategory"
     :layout="layout"
-    :display-mode-bar="false" />
+    :display-mode-bar="false" 
+    />
+</div>  
 
-  <h2>New vs Returning Visitors</h2>
-    <Plotly v-if="loaded"
-    :data="visitorsData"
-    :layout="layout"
-    :display-mode-bar="false" />
 
   </div>
 </template>
@@ -34,70 +30,66 @@ export default {
    data() {
         return {
             loaded: false, 
-            pageViewData:null, 
             pageViewsByCategoryData: null,
-            visitorsData: null,
             layout: {
                 autosize: false, 
                 width:750,
                 barmode:'stack'
-            }
+            },
+            menuItem:''
         }
 },
   async created() {
-        let resp = await fetch("https://api.jsonbin.io/b/5e2b4f673d75894195de48ff/1");
+        let resp = await fetch("https://v2-api.sheety.co/fb879a747168332af39e699cb4ca1d0f/menudp/mcdonalds/");
         let data = await resp.json();
 
-        let pageViews = data.pageViews;
-        let pageViewsByCategory = data.pageViewsByCategory;
-        let visitors = data.newVsReturning;
+        let breakfast = data.mcdonalds.filter(m => m.category === "Breakfast")
+        //let pageViewsByCategory = data.pageViewsByCategory;
 
         // setup data for line chart
-        this.pageViewData = [
-            { x:[], y:[], type:'scatter' }
-        ];
-
-        pageViews.forEach(pv => {
-            this.pageViewData[0].x.push(pv.date);
-            this.pageViewData[0].y.push(pv.views);
-        });
+        console.log(breakfast)
 
         // setup data for pie chart
-        this.pageViewsByCategoryData = [
+        this.nutritionByCategory = [
             {
-                values: [],
-                labels: [], 
-                type: 'pie'
+                values: [234,456,456],
+                labels: ['l','n','d'], 
+                marker:{ colors:[
+                  '#AF832D',
+                  '#75635B',
+                  '#C1B5A8'
+                ]
+                },
+                type: 'pie',
+                showlegend: false,
+                hole:.4
             }
         ];
+        let food = breakfast.filter(br => br.item ==="Egg McMuffin")[0]
+        this.menuItem = food.item
+        //let filter =['protein']
+        const foodFilter = Object.values(food)
+        console.log(foodFilter)
+        console.log(foodFilter.reduce((acu, cV) => {console.log(acu, cV)},{}) ) 
 
-        for(let cat in pageViewsByCategory) {
-            this.pageViewsByCategoryData[0].labels.push(cat);
-            this.pageViewsByCategoryData[0].values.push(pageViewsByCategory[cat]);
-        }
+        //for (let f in food){
+          
+          //this.nutritionByCategory[0].labels.push(f)
+          //this.nutritionByCategory[0].values.push(food[f])
+        //}
 
-        this.visitorsData = [
-            { 
-                x: [], 
-                y: [],
-                name:'New Users',
-                type:'bar'
-            },
-            { 
-                x: [], 
-                y: [],
-                name:'Old Users',
-                type:'bar'
-            }
-        ]
+
+        /*for(let cat in pageViewsByCategory) {
+           
+            console.log(pageViewsByCategory, cat)
+            
+            this.pageViewsByCategoryData[0].labels.push("dummy");
+            this.pageViewsByCategoryData[0].values.push(4);
+        }*/
+
 
         // setup data for stacked bar
-        visitors.forEach(v => {
-            this.visitorsData[0].x.push(v.date);
-            this.visitorsData[0].y.push(v.new);
-            this.visitorsData[1].x.push(v.date);
-            this.visitorsData[1].y.push(v.old);
-        });
+        
 
         this.loaded = true;
     }
